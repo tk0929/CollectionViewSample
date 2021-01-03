6cb1550c43d54f037e0e6373201eea75f940814c
tk0929 <bluecoffin929@gmail.com>
2021-01-03 23:28:25 +0900

    [add] CollectionViewを実装

--- CollectionViewSample/ViewController.swift
+++ CollectionViewSample/ViewController.swift
@@ -9,11 +9,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectonView: UICollectionView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+//        self.collectonView.delegate = self
+        self.collectonView.dataSource = self
+        
+        
     }
 
 
 }
 
+// MARK: - UICollectonViewDataSource
+
+extension ViewController: UICollectionViewDataSource{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = self.collectonView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.backgroundColor = .red
+        return cell
+    }
+    
+}
+
+// MARK: - UICollectonViewDelegate
+
+extension ViewController: UICollectionViewDelegate{
+    
+    
+    
+}
